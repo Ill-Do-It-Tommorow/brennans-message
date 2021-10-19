64ca5f061bcbcc0436f61c8b9c533351480235ff
Ill-Do-It-Tommorow <92817815+Ill-Do-It-Tommorow@users.noreply.github.com>
2021-10-19 14:06:55 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+basic.forever(function () {
+    basic.showString("Brennan")
+    basic.showString("Knows")
+    basic.showString("Where")
+    basic.showString("You")
+    basic.showString("Sleep")
+})
